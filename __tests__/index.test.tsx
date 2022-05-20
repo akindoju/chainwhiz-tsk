700e2c8b0c96390ce0ba662b3423938b127b0bd9
akindoju <davidd.akindoju@gmail.com>
2022-05-21 00:02:54 +0100

    Created snapshot

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,6 +2,7 @@ import { screen, render } from "@testing-library/react";
 import Home from "../pages/index";
 import "@testing-library/jest-dom";
 import { AddressContext } from "../contexts/AddressContext";
+import renderer from "react-test-renderer";
 
 describe("Home", () => {
   it("expect components to be rendered", () => {
@@ -22,5 +23,11 @@ describe("Home", () => {
       expect(separator).toBeInTheDocument();
     });
     expect(separators).toHaveLength(2);
+  });
+
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Home />).toJSON();
+
+    expect(tree).toMatchSnapshot();
   });
 });
